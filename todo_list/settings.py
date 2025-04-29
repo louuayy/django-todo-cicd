0114e7da919690d9ee570b6d645c1032c1ee20ec
louuayy <louay123medeni@gmail.com>
2025-04-29 23:36:15 +0100

    CI/CD test

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -12,7 +12,7 @@ DATABASES = {
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'd)=^c7!0-oqjmqve%(bt+p#sq6x*ipz2keh741j*-@f@_)f!1t')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
